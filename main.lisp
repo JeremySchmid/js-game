9b3e0d8c818394837640523622df305a811e61d7
Jeremy Schmid <dont-look-now@cox.net>
2016-10-05 22:42:17 -0700

    initializing and printing map successfully

--- main.lisp
+++ main.lisp
@@ -1,5 +1,5 @@
 (defun run-game ()
-  (load "source/game.lisp")
+  (load "../../source/projects/game/main.lisp")
   (start))
 
 (defvar *my-error-log*)
@@ -11,38 +11,36 @@
 
 (defparameter element-list '((1 (floor)) (2 (wall))))
 
-(defun initialize-map-area (width height)
-  (let ((area (make-array (list width height) :element-type 'fixnum :initial-element 1)))
-	 (do ((x 0 (1+ x)))
+(defun initialize-map-area (height width)
+  (let ((area (make-array (list height width) :element-type 'fixnum :initial-element 1)))
+	 (do ((y 0 (1+ y)))
+		((>= y height))
+		(do ((x 0 (1+ x)))
 		  ((>= x width))
-	   (do ((y 0 (1+ y)))
-		    ((>= y height))
 		  (if (or (= x 0) (= y 0) (= x (1- width)) (= y (1- height)))
-			 (setf (aref area x y) 2))))
+			 (setf (aref area y x) 2))))
 	 area))
 
-(defun print-square (row square)
-  (cond ((equal player (cons row square)) "@")
-		  ((= square 1) ".")
-		  ((= square 2) "0")
-		  (t " ")))
+(defun print-square (verts player-loc row column)
+  (let ((value (aref verts row column)))
+	 (format t (cond ((equal player-loc (cons row column)) "@")
+					  ((= value 1) " ")
+					  ((= value 2) "-")
+					  (t "e")))))
 
-(defun print-row ()
-  (mapcar #'print-square
+(defun print-row (verts player-loc row num-columns)
+  (loop for column from 0 to (1- num-columns)
+		  do (print-square verts player-loc row column)))
 
-(defun print-map-area (map-area player)
-  (loop for row from 0 to 
-		  do (loop for square in row
-					  do (let ((square-char (cond ((equal player (cons row square)) "@")
-														((= square 1) ".")
-														((= square 2) "0")
-														(t " "))))
-						 (princ square-char)))
-		  (princ #\NewLine)))
-
+(defun print-map-area (map-area player-loc)
+  (let ((num-rows (nth 1 map-area)))
+	 (loop for row from 0 to (1- num-rows)
+			 do (print-row (nth 0 map-area) player-loc row (nth 2 map-area))
+			 (format t "~%"))))
 
 (defun start ()
   (with-open-file (*my-error-log* "error.log" :direction :output :if-exists :supersede)
-	 (let ((map-area `(,(initialize-map-area 220 80) 220 80))
-			 (player '(75 . 25)))
+	 (let ((map-area (list (initialize-map-area 80 220) 80 220))
+			 (player '(25 . 75)))
+		(list map-area player)
 		(print-map-area map-area player))))
